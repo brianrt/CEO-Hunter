a8954a5411d2286daacc57e511f6ef46800c8106
Brian Thompson <brthomp2@illinois.edu>
2016-07-20 23:56:18 -0500

    Increased accuracy dramatically for phone and email recognition

--- send_links.js
+++ send_links.js
@@ -1,27 +1,37 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+var links;
+
+function getEmail(text){
+  var emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(emailRe.test(text)){
+    return text;
+  }
+  return "";
+}
+
+function getPhoneNumber(text){
+  var phoneRe = /\(?\d{3}\)?[-. –](\d{3})[-. –](\d{4})/;
+  if (phoneRe.test(text))
+    return text.match(phoneRe)[0];
+  return "";
+}
+
 
 // Send back to the popup a sorted deduped list of valid link URLs on this page.
 // The popup injects this script into all frames in the active tab.
-
-var links = [].slice.apply(document.getElementsByTagName('a'));
+links = [].slice.apply(document.querySelectorAll('p,li,a,div'));
 links = links.map(function(element) {
-  // Return an anchor's href attribute, stripping any URL fragment (hash '#').
-  // If the html specifies a relative path, chrome converts it to an absolute
-  // URL.
-  var href = element.href;
-  var hashIndex = href.indexOf('#');
-  if (hashIndex >= 0) {
-    href = href.substr(0, hashIndex);
+  var text = element.innerText;
+  var html = element.innerHTML;
+  var phone = getPhoneNumber(text)
+  if(phone.length>0){
+    return "Phone: "+phone;
   }
-  if (href.includes("mailto:")){
-    href = href.substr(7);
-    return href;
-  } 
-  else if (href.includes("tel:")){
-    href = href.substr(4);
-    return href;
+  var email = getEmail(text)
+  if(email.length>0){
+    return "Email: "+email;
   }
   return "";
 });
