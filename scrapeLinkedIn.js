cd882e5532702ac2b46940c9341e880c290ba841
Brian Thompson <brthomp2@illinois.edu>
2016-12-23 21:38:49 -0600

    One last bug fix

--- scrapeLinkedIn.js
+++ scrapeLinkedIn.js
@@ -7,12 +7,12 @@ for(var i = 0; i < sign_in.length;i++){
 	console.log('"'+sign_in[i].innerHTML.trim()+'"');
 }
 var sign_in_two = document.getElementsByClassName("title")[0];
-if(sign_in_two.innerHTML=="Make the most of your professional life"){
+if(sign_in_two!=undefined && sign_in_two.innerHTML=="Make the most of your professional life"){
 	alert("Please sign in to LinkedIn in a new tab. Then go back to the original webpage and try again.");
 	window.close();
 }
 var result = document.getElementsByClassName("more")[0];
-if(result==null){
+if(result==undefined){
 	result = document.getElementsByClassName("snackbar-description-see-all-link")[0];
 	console.log(result);
 }
