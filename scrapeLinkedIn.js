1b81fd08aaa40bc55b27848541e1d517b00494da
Brian Thompson <brthomp2@illinois.edu>
2017-04-17 14:51:11 -0500

    debugging version of linkedin update

--- scrapeLinkedIn.js
+++ scrapeLinkedIn.js
@@ -1,40 +1,50 @@
+function alertSignIn(){
+	alert("Please sign in to LinkedIn in a new tab. Then go back to the original webpage and try again.");
+	window.close();
+}
+
 var sign_in = document.getElementsByClassName("nav-link");
 for(var i = 0; i < sign_in.length;i++){
 	if(sign_in[i].innerHTML.trim()=="Sign In"){
-		alert("Please sign in to LinkedIn in a new tab. Then go back to the original webpage and try again.");
-		window.close();
+		alertSignIn();
 	}
 	console.log('"'+sign_in[i].innerHTML.trim()+'"');
 }
 var sign_in_two = document.getElementsByClassName("title")[0];
 if(sign_in_two!=undefined && sign_in_two.innerHTML=="Make the most of your professional life"){
-	alert("Please sign in to LinkedIn in a new tab. Then go back to the original webpage and try again.");
-	window.close();
+	alertSignIn();
 }
 var sign_in_three = document.getElementsByClassName("join-linkedin-form float-label-form")[0];
 if(sign_in_three!=undefined){
-	alert("Please sign in to LinkedIn in a new tab. Then go back to the original webpage and try again.");
-	window.close();
+	alertSignIn();
 }
 var html = document.body.innerHTML;
-console.log(html);
-if(html.indexOf("/search/results/people/?facet")==-1){
-	console.log("The link was not found, method doesn't work for this site");
-	//window.close();
+if(html.indexOf("Sign in")!=-1){
+	alertSignIn();
 }
-else{
-	console.log("It worked");
+// console.log(html);
+if(html.indexOf("/search/results/people/?facet")!=-1){ //found a link through method 1
 	var link = html.substring(html.indexOf("/search/results/people/?facet"));
 	link = "https://www.linkedin.com"+link.substring(0,link.indexOf("\""));
-	console.log(link);
+	console.log("here is the output link from method 1: "+link);
+	//window.close();
 }
-// var link = html.substring(html.indexOf("/search/results/people/?facet"));
-// if(link==null){
-// 	console.log("The link was null, method doesn't work for this site");
-// }
-// else{
-// 	link = "https://www.linkedin.com"+link.substring(0,link.indexOf("\""));
-// 	console.log(link);
+else if(html.indexOf("https://www.linkedin.com/vsearch/p?f_CC=")!=-1){ // try method 2 with vsearch without the ";"
+	console.log("method 1 didn't work, trying method 2...");
+	var link = html.substring(html.indexOf("https://www.linkedin.com/vsearch/p?f_CC="));
+	var result_link = link.substring(0,link.indexOf("\""));
+	while(result_link.indexOf(";")!=-1){ //while there is a semicolon in the result, we want to search for the next one
+		link = link.substring(40); //cutoff the virst vsearch search result
+		link = link.substring(link.indexOf("https://www.linkedin.com/vsearch/p?f_CC="));
+		result_link = link.substring(0,link.indexOf("\""));
+		console.log("current search item: "+result_link);
+	}
+	console.log("here is the output link from method 2: "+result_link);
+}
+else{
+	console.log("Neither method worked :(");
+}
+
 // 	chrome.runtime.sendMessage({
 // 		greeting: "company linkedin page",
 // 		message: link
